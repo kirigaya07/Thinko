353320af0f39995ef8dac6d12641d8209b238614
Anmol Chandrakar <crimsomemoon7@gmail.com>
2025-08-18 22:45:24 +0530

    Add authentication handling to Heading and Navbar components; introduce Spinner component for loading states

--- app/(landing)/_components/Heading.tsx
+++ app/(landing)/_components/Heading.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl">
@@ -11,13 +17,29 @@ export const Heading = () => {
         <span className="underline">Jotion</span>
       </h1>
       <h3 className="text-base font-medium sm:text-xl md:text-2xl">
-        Jotion is the connected workspace where <br /> better, faster work
+        Thinko is the connected workspace where <br /> better, faster work
         happens.
       </h3>
-      <Button>
-        Enter Jotion
-        <ArrowRight className="ml-2 h-4 w-4" />
-      </Button>
+      {isLoading && (
+        <div className="flex w-full items-center justify-center">
+          <Spinner />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents" />
+          Enter Jotion
+          <ArrowRight className="ml-2 h-4 w-4" />
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Jotion free
+            <ArrowRight className="ml-2 h-4 w-4" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
