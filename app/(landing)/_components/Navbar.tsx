1597b1f077b5fa3eaf4df579569e99e28985ae68
Anmol Chandrakar <crimsomemoon7@gmail.com>
2025-08-18 23:09:00 +0530

    Update Navbar and UserItem components to replace

--- app/(landing)/_components/Navbar.tsx
+++ app/(landing)/_components/Navbar.tsx
@@ -32,14 +32,14 @@ export const Navbar = () => {
               </Button>
             </SignInButton>
             <SignInButton mode="modal">
-              <Button size="sm">Get Jotion Free</Button>
+              <Button size="sm">Get Thinko Free</Button>
             </SignInButton>
           </>
         )}
         {isAuthenticated && !isLoading && (
           <>
             <Button variant="ghost" size="sm" asChild>
-              <Link href="/documents"> Enter Jotion </Link>
+              <Link href="/documents"> Enter Thinko </Link>
             </Button>
             <UserButton afterSignOutUrl="/" />
           </>
