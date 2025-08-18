477fe76b8e82c3128abcdda01964fd7368b1eef2
Anmol Chandrakar <crimsomemoon7@gmail.com>
2025-08-18 22:59:12 +0530

    Add Toaster component; introduce UserItem component for user profile dropdown functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,12 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Thinko",
+  title: "Jotion",
   description:
     "The seamless platform where creative and productive work happens.",
   icons: {
@@ -42,6 +43,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey="jotion-theme-2"
           >
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
