0d7c0438f5b015909a5a153df500ab8f988d9576
Anmol Chandrakar <crimsomemoon7@gmail.com>
2025-08-21 23:50:24 +0530

    Update error and heading components: replace PNG images with SVGs for better scalability, and adjust heading structure for improved semantics.

--- app/(landing)/_components/Heading.tsx
+++ app/(landing)/_components/Heading.tsx
@@ -12,14 +12,14 @@ export const Heading = () => {
 
   return (
     <div className="max-w-3xl space-y-4">
-      <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl">
-        Your Ideas, Documents, & Plans. Welcome to{" "}
-        <span className="underline">Jotion</span>
+      <h1 className="text-3xl font-bold sm:text-5xl md:text-5xl">
+        Your Ideas💡, Documents📕, & Plans🚀. Welcome to{" "}
+        <span className="underline">Thinko</span>
       </h1>
-      <h3 className="text-base font-medium sm:text-xl md:text-2xl">
+      <h2 className="text-base font-medium sm:text-xl">
         Thinko is the connected workspace where <br /> better, faster work
         happens.
-      </h3>
+      </h2>
       {isLoading && (
         <div className="flex w-full items-center justify-center">
           <Spinner size="md" />
@@ -28,7 +28,7 @@ export const Heading = () => {
       {isAuthenticated && !isLoading && (
         <Button asChild>
           <Link href="/documents">
-            Enter Jotion
+            Enter Thinko
             <ArrowRight className="ml-2 h-4 w-4" />
           </Link>
         </Button>
@@ -36,7 +36,7 @@ export const Heading = () => {
       {!isAuthenticated && !isLoading && (
         <SignInButton mode="modal">
           <Button>
-            Get Jotion free
+            Get Thinko free
             <ArrowRight className="ml-2 h-4 w-4" />
           </Button>
         </SignInButton>
