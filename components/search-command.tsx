096bd427cce86532d80dc5a4982c3b6cfd04df1e
Anmol Chandrakar <crimsomemoon7@gmail.com>
2025-08-21 23:57:56 +0530

    Add ModeToggle component: implement a theme toggle feature using a dropdown menu with light, dark, and system options for improved user experience.

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -44,8 +44,7 @@ export const SearchCommand = () => {
   }, [toggle]);
 
   const onSelect = (id: string) => {
-    const splitId = id.split("-")[0];
-    router.push(`/documents/${splitId}`);
+    router.push(`/documents/${id}`);
     onClose();
   };
 
@@ -62,7 +61,7 @@ export const SearchCommand = () => {
           {documents?.map((document) => (
             <CommandItem
               key={document._id}
-              value={`${document._id}-${document.title}`}
+              value={document._id}
               title={document.title}
               onSelect={onSelect}
             >
