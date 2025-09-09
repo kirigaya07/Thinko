a8dc553b22811ae11a26462faaff1675094b9a52
Anmol Chandrakar <crimsomemoon7@gmail.com>
2025-09-10 01:00:43 +0530

    Update package dependencies: add @vercel/speed-insights and autoprefixer, and update postcss version. Integrate SpeedInsights component in layout for performance monitoring.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { EdgeStoreProvider } from "@/lib/edgestore";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -67,6 +68,7 @@ export default function RootLayout({
             >
               <Toaster position="bottom-center" />
               <ModalProvider />
+              <SpeedInsights />
               {children}
             </ThemeProvider>
           </EdgeStoreProvider>
