506ab9cab1f82b587f47915f15c3d697fd20b52f
Anmol Chandrakar <crimsomemoon7@gmail.com>
2025-09-09 23:33:51 +0530

    Update Next.js configuration and TypeScript settings: add transpilePackages for Blocknote libraries and change moduleResolution to 'node' for improved compatibility.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ["@blocknote/core", "@blocknote/mantine", "@blocknote/react"],
   images: {
     remotePatterns: [
       {
